c8280a25704da6a6793a92f495ba095ed3f1dcec
bohenmian <bohenmian@gmail.com>
2018-06-12 15:50:51 +0800

    [ADD]文件服务新增接口,根据bucket,filename返回文件上传相对路径

--- src/main/java/io/choerodon/file/app/service/impl/FileServiceImpl.java
+++ src/main/java/io/choerodon/file/app/service/impl/FileServiceImpl.java
@@ -1,6 +1,7 @@
 package io.choerodon.file.app.service.impl;
 
 import io.choerodon.core.exception.CommonException;
+import io.choerodon.file.api.dto.FileDTO;
 import io.choerodon.file.app.service.FileService;
 import io.minio.MinioClient;
 import io.minio.policy.PolicyType;
@@ -15,7 +16,7 @@ import java.io.InputStream;
 import java.util.UUID;
 
 /**
- * @author  HuangFuqiang@choerodon.io
+ * @author HuangFuqiang@choerodon.io
  */
 @Service
 public class FileServiceImpl implements FileService {
@@ -76,5 +77,27 @@ public class FileServiceImpl implements FileService {
             LOGGER.info(e.getMessage());
             throw new CommonException(e.getMessage());
         }
+    }
+
+    @Override
+    public FileDTO uploadDocument(String bucketName, String originFileName, MultipartFile multipartFile) {
+        try {
+            MinioClient minioClient = new MinioClient(endpoint, accessKey, secretKey);
+            boolean isExist = minioClient.bucketExists(bucketName);
+            if (isExist) {
+                LOGGER.debug("Bucket already exists.");
+            } else {
+                minioClient.makeBucket(bucketName);
+                minioClient.setBucketPolicy(bucketName, "", PolicyType.READ_ONLY);
+            }
+            InputStream inputStream = multipartFile.getInputStream();
+            String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+            String fileName = uuid + "_" + originFileName;
+            minioClient.putObject(bucketName, fileName, inputStream, "application/octet-stream");
+            return new FileDTO(endpoint, originFileName, fileName);
+        } catch (Exception e) {
+            LOGGER.info(e.getMessage());
+        }
+        return null;
     }
 }
