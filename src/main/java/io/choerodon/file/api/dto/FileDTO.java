51306a02a5cee6b9c5305a796cea1e6f3a8ec2b1
Fan <fan@choerodon.io>
2018-08-21 09:51:38 +0800

    [IMP] add dto ApiModelProperty

--- src/main/java/io/choerodon/file/api/dto/FileDTO.java
+++ src/main/java/io/choerodon/file/api/dto/FileDTO.java
@@ -1,9 +1,14 @@
 package io.choerodon.file.api.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class FileDTO {
 
+    @ApiModelProperty("文件服务器地址")
     private String endPoint;
+    @ApiModelProperty("原始文件名")
     private String originFileName;
+    @ApiModelProperty("新文件名")
     private String fileName;
 
     public FileDTO(String endPoint, String originFileName, String fileName) {
