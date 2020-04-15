0b9ca648f71c660abaa8e941b290cad35209bf37
scp <changping.shi@hand-china.com>
2020-04-15 11:18:17 +0800

    [IMP] 修改pom依赖

--- src/main/java/io/choerodon/file/FileApplication.java
+++ src/main/java/io/choerodon/file/FileApplication.java
@@ -1,5 +1,7 @@
 package io.choerodon.file;
 
+import org.hzero.autoconfigure.file.EnableHZeroFile;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
