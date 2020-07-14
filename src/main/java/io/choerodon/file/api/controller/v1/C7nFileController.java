91ba012c03836e465f8f35d5005c48c3fefc19ae
zmf <1984654893@qq.com>
2020-07-14 09:39:44 +0800

    [REM] remove devops file upload API

--- src/main/java/io/choerodon/file/api/controller/v1/C7nFileController.java
+++ src/main/java/io/choerodon/file/api/controller/v1/C7nFileController.java
@@ -1,18 +1,17 @@
 package io.choerodon.file.api.controller.v1;
 
+import java.util.List;
+
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import io.choerodon.core.iam.ResourceLevel;
 import io.choerodon.file.app.service.FileC7nService;
 import io.choerodon.swagger.annotation.Permission;
-
-import java.util.List;
 
 /**
  * @author zmf
@@ -23,39 +22,6 @@ import java.util.List;
 public class C7nFileController {
     @Autowired
     private FileC7nService fileC7nService;
-
-    /**
-     * 上传DevOps的CI过程产生的软件包
-     *
-     * @param tenantId      租户id
-     * @param token         应用服务token
-     * @param commit        此次ci的commit sha值
-     * @param ciPipelineId  流水线id
-     * @param ciJobId       流水线的job id
-     * @param artifactName  软件包名称
-     * @param multipartFile 软件包
-     * @return 软件包地址， 如果有异常，返回400
-     */
-    @Permission(permissionPublic = true, level = ResourceLevel.SITE)
-    @ApiOperation(value = "上传DevOps的CI软件包文件")
-    @PostMapping("/devops/files")
-    public ResponseEntity<String> uploadDevOpsArtifactFile(
-            @ApiParam(value = "租户id")
-            @RequestParam(value = "tenant_id") Long tenantId,
-            @ApiParam(value = "应用服务token", required = true)
-            @RequestParam(value = "token") String token,
-            @ApiParam(value = "此次ci的commit", required = true)
-            @RequestParam(value = "commit") String commit,
-            @ApiParam(value = "gitlab内置的流水线id", required = true)
-            @RequestParam(value = "ci_pipeline_id") Long ciPipelineId,
-            @ApiParam(value = "gitlab内置的jobId", required = true)
-            @RequestParam(value = "ci_job_id") Long ciJobId,
-            @ApiParam(value = "ci流水线定义的软件包名称", required = true)
-            @RequestParam(value = "artifact_name") String artifactName,
-            @ApiParam(value = "上传文件", required = true)
-            @RequestParam("file") MultipartFile multipartFile) {
-        return ResponseEntity.ok(fileC7nService.uploadDevOpsArtifactFile(tenantId, token, commit, ciPipelineId, ciJobId, artifactName, multipartFile));
-    }
 
 
     @Permission(permissionPublic = true, level = ResourceLevel.SITE)
--- src/main/java/io/choerodon/file/app/service/impl/FileC7nServiceImpl.java
+++ src/main/java/io/choerodon/file/app/service/impl/FileC7nServiceImpl.java
@@ -1,37 +1,26 @@
 package io.choerodon.file.app.service.impl;
 
-import java.util.Collections;
 import java.util.List;
 
-import org.hzero.core.base.BaseConstants;
 import org.hzero.file.api.dto.FileDTO;
 import org.hzero.file.app.service.CapacityUsedService;
 import org.hzero.file.app.service.FileService;
-import org.hzero.file.domain.entity.File;
 import org.hzero.file.domain.repository.FileRepository;
 import org.hzero.file.domain.service.factory.StoreFactory;
 import org.hzero.file.domain.service.factory.StoreService;
 import org.hzero.file.infra.util.CodeUtils;
-import org.hzero.mybatis.domian.Condition;
-import org.hzero.mybatis.util.Sqls;
 import org.hzero.starter.file.service.AbstractFileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
-import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import io.choerodon.core.exception.CommonException;
 import io.choerodon.file.app.service.FileC7nService;
-import io.choerodon.file.infra.constant.DevOpsConstants;
-import io.choerodon.file.infra.exception.DevopsCiInvalidException;
-import io.choerodon.file.infra.feign.DevOpsClient;
 import io.choerodon.file.infra.utils.ImageUtils;
 
 /**
@@ -45,8 +34,6 @@ public class FileC7nServiceImpl implements FileC7nService {
 
     @Autowired
     private FileService fileService;
-    @Autowired
-    private DevOpsClient devOpsClient;
     @Autowired
     private StoreFactory storeFactory;
     @Autowired
@@ -62,70 +49,6 @@ public class FileC7nServiceImpl implements FileC7nService {
         } catch (Exception e) {
             throw new CommonException("error.cut.and.upload.image", e);
         }
-    }
-
-    @Override
-    public String uploadDevOpsArtifactFile(Long tenantId, String token, String commit, Long ciPipelineId, Long ciJobId, String artifactName, MultipartFile multipartFile) {
-        // 为了抛出非2xx的状态码让客户端感知到
-        try {
-            return doUploadDevOpsArtifactFile(tenantId, token, commit, ciPipelineId, ciJobId, artifactName, multipartFile);
-        } catch (DevopsCiInvalidException ex) {
-            throw ex;
-        } catch (Exception ex) {
-            throw new DevopsCiInvalidException(ex.getMessage(), ex);
-        }
-    }
-
-    public String doUploadDevOpsArtifactFile(Long tenantId, String token, String commit, Long ciPipelineId, Long ciJobId, String artifactName, MultipartFile multipartFile) {
-        LOGGER.info("UploadDevOpsArtifactFile: the tenant id {}, token {}, commit {}, ciPipelineId {}, ciJobId: {}, artifactName: {}, size: {}", tenantId, token, commit, ciPipelineId, ciJobId, artifactName, multipartFile.getSize());
-        String fileName = String.format(DevOpsConstants.CI_JOB_ARTIFACT_NAME_TEMPLATE, ciPipelineId, artifactName);
-        // 先去devops-service校验参数
-        ResponseEntity<Boolean> checkResult = devOpsClient.checkJobArtifactInfo(token, commit, ciPipelineId, ciJobId, artifactName, multipartFile.getSize());
-        if (checkResult.getStatusCode() != HttpStatus.OK || !Boolean.TRUE.equals(checkResult.getBody())) {
-            throw new DevopsCiInvalidException("artifact.information.invalid");
-        }
-
-        // 查询是否已经有这个文件
-        // 这个查询从数据库来说是不保证唯一性的, 但是从这个逻辑这里保证了唯一性
-        // 除非有另一处上传文件的地方使用了相同的bucket和命令规则, 否则, 不会出现误删除文件的情况
-        List<File> files = fileRepository.selectByCondition(Condition.builder(File.class).where(
-                Sqls.custom()
-                        .andEqualTo(File.FIELD_FILE_NAME, fileName)
-                        .andEqualTo(File.FIELD_TENANT_ID, tenantId)
-                        .andEqualTo(File.FIELD_BUCKET_NAME, DevOpsConstants.DEV_OPS_CI_ARTIFACT_FILE_BUCKET)
-        ).build());
-
-        if (!CollectionUtils.isEmpty(files)) {
-            if (files.size() > 1) {
-                LOGGER.warn("Unexpected size of the files with name {} in doUploadDevOpsArtifactFile. The size is {}", fileName, files.size());
-            }
-            for (File dbRecord : files) {
-                // 有的话先删除了, 保证唯一性
-                if (dbRecord != null && dbRecord.getFileKey() != null) {
-                    fileService.deleteFileByKey(tenantId, dbRecord.getFileKey());
-                }
-            }
-        }
-
-        // 上传文件
-        String fileUrl = fileService.uploadMultipart(tenantId, DevOpsConstants.DEV_OPS_CI_ARTIFACT_FILE_BUCKET, null, null, fileName, BaseConstants.Flag.NO, null, multipartFile);
-
-        // 将信息给devops-service保存
-        ResponseEntity<Void> responseEntity;
-        try {
-            responseEntity = devOpsClient.saveJobArtifactInfo(token, commit, ciPipelineId, ciJobId, artifactName, fileUrl);
-        } catch (Exception e) {
-            fileService.deleteByUrls(tenantId, DevOpsConstants.DEV_OPS_CI_ARTIFACT_FILE_BUCKET, null, Collections.singletonList(fileUrl));
-            throw new DevopsCiInvalidException("error.save.job.artifact.information", artifactName, e);
-        }
-
-        // 判断响应
-        if (responseEntity == null || responseEntity.getStatusCode() != HttpStatus.OK) {
-            fileService.deleteByUrls(tenantId, DevOpsConstants.DEV_OPS_CI_ARTIFACT_FILE_BUCKET, null, Collections.singletonList(fileUrl));
-            throw new DevopsCiInvalidException("error.save.job.artifact.information", artifactName);
-        }
-        LOGGER.info("Finished UploadDevOpsArtifactFile: the tenant id {}, token {}, commit {}, ciPipelineId {}, ciJobId: {}, artifactName: {}, size: {}", tenantId, token, commit, ciPipelineId, ciJobId, artifactName, multipartFile.getSize());
-        return fileUrl;
     }
 
     @Override
