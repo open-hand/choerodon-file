87080cc07dbe8fbecf42632d4e0487d47d1a3410
zmf <1984654893@qq.com>
2020-07-14 10:41:17 +0800

    [ADD] encrypt primary key

--- src/main/java/io/choerodon/file/api/controller/v1/C7nFileController.java
+++ src/main/java/io/choerodon/file/api/controller/v1/C7nFileController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.hzero.starter.keyencrypt.core.Encrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,9 +28,11 @@ public class C7nFileController {
     @Permission(permissionPublic = true, level = ResourceLevel.SITE)
     @ApiOperation(value = "根据bucketName和文件的完全url删除文件")
     @PostMapping("/{organizationId}/delete-by-url")
-    public ResponseEntity deleteByUrls(@ApiParam(value = "租户ID", required = true) @PathVariable Long organizationId,
-                                       @ApiParam(value = "桶名", required = true) @RequestParam("bucketName") String bucketName,
-                                       @ApiParam(value = "文件地址", required = true) @RequestBody List<String> urls) {
+    public ResponseEntity deleteByUrls(
+            @Encrypt
+            @ApiParam(value = "租户ID", required = true) @PathVariable Long organizationId,
+            @ApiParam(value = "桶名", required = true) @RequestParam("bucketName") String bucketName,
+            @ApiParam(value = "文件地址", required = true) @RequestBody List<String> urls) {
         fileC7nService.deleteByUrls(organizationId, bucketName, urls);
         return ResponseEntity.ok(HttpStatus.OK);
     }
