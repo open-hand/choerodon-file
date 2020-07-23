55fd0c771d26f38e71cbe05d37471e540fa8455d
王灏 <2507172753@qq.com>
2020-07-23 10:30:58 +0800

    [imp]修改数据修复groovy

--- src/main/resources/script/db/groovy/hzero_file/datafix_for_hzero_1_4_0.groovy
+++ src/main/resources/script/db/groovy/hzero_file/datafix_for_hzero_1_4_0.groovy
@@ -2,6 +2,10 @@ package script.db
 
 databaseChangeLog(logicalFilePath: 'script/db/datafix_for_hzero_1_4_0.groovy') {
     changeSet(author: 'wanghao', id: '2020-07-21-data-fix') {
+        preConditions(onFail: "MARK_RAN") {
+            tableExists(tableName: "hfle_file_edit_log")
+            tableExists(tableName: "hfle_file")
+        }
         sql("""
             UPDATE hfle_file_edit_log hfel
             SET hfel.tenant_id = ( SELECT hf.tenant_id FROM hfle_file hf WHERE hfel.file_id = hf.file_id)
