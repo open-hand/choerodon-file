12069e5511de407e1a22c606f1d53be196aa8c2f
18986 <changping.shi@hand-chian.com>
2021-03-25 16:15:31 +0800

    [IMP] 修改初始化数据配置

--- init-database.sh
+++ init-database.sh
@@ -2,17 +2,16 @@
 MAVEN_LOCAL_REPO=$(cd / && mvn help:evaluate -Dexpression=settings.localRepository -q -DforceStdout)
 TOOL_GROUP_ID=io.choerodon
 TOOL_ARTIFACT_ID=choerodon-tool-liquibase
-TOOL_VERSION=0.19.1-SNAPSHOT
+TOOL_VERSION=0.19.1.RELEASE
 TOOL_JAR_PATH=${MAVEN_LOCAL_REPO}/${TOOL_GROUP_ID/\./\/}/${TOOL_ARTIFACT_ID}/${TOOL_VERSION}/${TOOL_ARTIFACT_ID}-${TOOL_VERSION}.jar
 mvn org.apache.maven.plugins:maven-dependency-plugin:get \
  -Dartifact=${TOOL_GROUP_ID}:${TOOL_ARTIFACT_ID}:${TOOL_VERSION} \
  -Dtransitive=false
 
-java -DSPRING_DATASOURCE_URL="jdbc:mysql://localhost:3306?serverTimezone=CTT&useUnicode=true&characterEncoding=utf-8&useSSL=false&useInformationSchema=true&remarks=true" \
- -DSPRING_DATASOURCE_USERNAME=root \
- -DSPRING_DATASOURCE_PASSWORD=root \
+java -Dspring.datasource.url="jdbc:mysql://localhost:3306?serverTimezone=CTT&useUnicode=true&characterEncoding=utf-8&useSSL=false&useInformationSchema=true&remarks=true" \
+ -Dspring.datasource.username=root \
+ -Dspring.datasource.password=root \
  -Ddata.init=true \
- -Dlogging.level.root=info \
  -Dinstaller.jarPath=target/app.jar \
  -jar ${TOOL_JAR_PATH}
 
