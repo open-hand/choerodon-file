4fc5e51d8ed4e954536fcb1001b24fb43901857d
18986 <changping.shi@hand-chian.com>
2021-09-22 15:14:35 +0800

    [ADD] 支持阿里云和华为云oss

--- src/main/java/io/choerodon/file/app/task/FileCommandRunner.java
+++ src/main/java/io/choerodon/file/app/task/FileCommandRunner.java
@@ -5,10 +5,12 @@ import org.hzero.file.domain.repository.StorageConfigRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
+
+import io.choerodon.file.infra.config.OssProperties;
+import io.choerodon.file.infra.enums.OssTypeEnum;
 
 /**
  * Creator: ChangpingShi0213@gmail.com
@@ -20,28 +22,14 @@ public class FileCommandRunner implements CommandLineRunner {
     private static final Logger LOGGER = LoggerFactory.getLogger(FileCommandRunner.class);
     @Autowired
     private StorageConfigRepository storageConfigRepository;
-
-    @Value("${minio.endpoint}")
-    private String endpoint;
-
-    @Value("${minio.accessKey}")
-    private String accessKey;
-
-    @Value("${minio.secretKey}")
-    private String secretKey;
-
-    @Value("${minio.access_control:read-write}")
-    private String accessControl;
-
-    @Value("${minio.storage_code:MINIO}")
-    private String storageCode;
-
-    @Value("${minio.update:true}")
-    private Boolean minioUpdate;
+    @Autowired
+    private OssProperties ossProperties;
+    private static final String STORAGE_CODE_FORMAT = "choerodon-%s";
 
     @Override
     public void run(String... strings) {
-        if (minioUpdate) {
+        if (ossProperties.getUpdate()) {
+            String storageCode = String.format(STORAGE_CODE_FORMAT, ossProperties.getType());
             StorageConfig queryDTO = new StorageConfig();
             queryDTO.setStorageCode(storageCode);
             queryDTO.setTenantId(0L);
@@ -49,16 +37,19 @@ public class FileCommandRunner implements CommandLineRunner {
             if (ObjectUtils.isEmpty(storageConfig)) {
                 storageConfig = new StorageConfig();
             }
+
+            OssTypeEnum typeEnum = OssTypeEnum.valueOf(ossProperties.getType());
             storageConfig.setTenantId(0L);
-            storageConfig.setAccessControl(accessControl);
-            storageConfig.setAccessKeyId(accessKey);
-            storageConfig.setAccessKeySecret(secretKey);
-            storageConfig.setEndPoint(endpoint);
+            storageConfig.setAccessControl(typeEnum.getAccessControl());
+            storageConfig.setAccessKeyId(ossProperties.getAccessKey());
+            storageConfig.setAccessKeySecret(ossProperties.getSecretKey());
+            storageConfig.setEndPoint(ossProperties.getEndpoint());
             storageConfig.setStorageCode(storageCode);
-            storageConfig.setBucketPrefix("");
+            storageConfig.setBucketPrefix(ossProperties.getPrefix());
             storageConfig.setDefaultFlag(1);
-            storageConfig.setStorageType(3);
+            storageConfig.setStorageType(typeEnum.getTypeNum());
             storageConfig.setCreateBucketFlag(1);
+            storageConfig.setRegion(ossProperties.getRegion());
             storageConfigRepository.insertOrUpdateStorageConfig(storageConfig);
         }
     }
