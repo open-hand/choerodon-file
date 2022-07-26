3f2ded870c8851c0f7c337d9903bcd6efeefc786
史常萍 <changping.shi@zknow.com>
2022-07-26 10:58:09 +0800

    [IMP] 修改slice文件上传

--- src/main/java/io/choerodon/file/app/service/impl/FileHzeroServiceImpl.java
+++ src/main/java/io/choerodon/file/app/service/impl/FileHzeroServiceImpl.java
@@ -1,7 +1,17 @@
 package io.choerodon.file.app.service.impl;
 
-import io.choerodon.core.exception.CommonException;
+import static io.choerodon.file.infra.constant.CommonConstant.FOLDER;
+import static io.choerodon.file.infra.constant.CommonConstant.STORAGE_CODE_FORMAT;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javax.validation.Validator;
+
 import org.apache.commons.lang3.StringUtils;
+import org.hzero.core.base.BaseConstants;
 import org.hzero.core.util.ValidUtils;
 import org.hzero.file.app.service.UploadConfigService;
 import org.hzero.file.app.service.impl.FileServiceImpl;
@@ -13,18 +23,13 @@ import org.hzero.file.domain.service.factory.StoreService;
 import org.hzero.file.infra.constant.HfleConstant;
 import org.hzero.file.infra.constant.HfleMessageConstant;
 import org.hzero.file.infra.util.ContentTypeUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 import org.springframework.util.DigestUtils;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Validator;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import io.choerodon.core.exception.CommonException;
+import io.choerodon.file.infra.config.OssProperties;
 
 /**
  * 文件服务实现类
@@ -42,6 +47,8 @@ public class FileHzeroServiceImpl extends FileServiceImpl {
     private Validator validator;
     @Autowired
     private UploadConfigHzeroServiceImpl uploadConfigHzeroService;
+    @Autowired
+    private OssProperties ossProperties;
 
     public FileHzeroServiceImpl(StoreFactory factory,
                                 FileRepository fileRepository,
@@ -80,6 +87,38 @@ public class FileHzeroServiceImpl extends FileServiceImpl {
             throw e;
         } catch (Exception e) {
             throw new CommonException(HfleMessageConstant.ERROR_FILE_UPDATE, e);
+        }
+    }
+
+    @Override
+    public Integer checkSlice(Long tenantId, String bucketName, String storageCode, Integer chunk, String guid) {
+        storageCode = getStorageCode(storageCode);
+        return super.checkSlice(tenantId, bucketName, storageCode, chunk, guid);
+    }
+
+    @Override
+    public void uploadSlice(Long tenantId, String bucketName, String directory, String storageCode, String filename, String contentType, Integer chunk, String guid, MultipartFile multipartFile) {
+        storageCode = getStorageCode(storageCode);
+        super.uploadSlice(tenantId, bucketName, directory, storageCode, filename, contentType, chunk, guid, multipartFile);
+    }
+
+    @Override
+    public void uploadByteSlice(Long tenantId, String bucketName, String directory, String storageCode, String filename, String contentType, Integer chunk, String guid, byte[] byteFile) {
+        storageCode = getStorageCode(storageCode);
+        super.uploadByteSlice(tenantId, bucketName, directory, storageCode, filename, contentType, chunk, guid, byteFile);
+    }
+
+    @Override
+    public String combineSlice(Long tenantId, String bucketName, String directory, String storageCode, String filename, Long filesize, String contentType, String guid, String attachmentUuid) {
+        storageCode = getStorageCode(storageCode);
+        return super.combineSlice(tenantId, bucketName, directory, storageCode, filename, filesize, contentType, guid, attachmentUuid);
+    }
+
+    private String getStorageCode(String storageCode) {
+        if (StringUtils.isEmpty(storageCode)) {
+            return String.format(STORAGE_CODE_FORMAT, ossProperties.getType() + BaseConstants.Symbol.MIDDLE_LINE + FOLDER);
+        } else {
+            return storageCode;
         }
     }
 }
