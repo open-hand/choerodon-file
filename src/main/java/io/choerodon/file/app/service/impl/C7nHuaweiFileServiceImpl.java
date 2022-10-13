91b28bbae7f3431136d68660293d9b22fd5c26e5
史常萍 <changping.shi@zknow.com>
2022-10-13 12:53:28 +0800

    [IMP] 修改文件上传带+问题

--- src/main/java/io/choerodon/file/app/service/impl/C7nHuaweiFileServiceImpl.java
+++ src/main/java/io/choerodon/file/app/service/impl/C7nHuaweiFileServiceImpl.java
@@ -1,5 +1,8 @@
 package io.choerodon.file.app.service.impl;
 
+import java.io.InputStream;
+import java.util.Map;
+
 import org.hzero.starter.file.entity.FileInfo;
 import org.hzero.starter.file.service.HuaweiFileServiceImpl;
 
@@ -8,9 +11,30 @@ import org.hzero.starter.file.service.HuaweiFileServiceImpl;
  * @since 2022/10/13
  */
 public class C7nHuaweiFileServiceImpl extends HuaweiFileServiceImpl {
+
+
     @Override
     public String upload(FileInfo file, String filePath) {
         String fileUrl = super.upload(file, filePath);
         return fileUrl.replace("+", "%2B");
     }
+
+    @Override
+    public String upload(FileInfo file, InputStream inputStream) {
+        String fileUrl = super.upload(file, inputStream);
+        return fileUrl.replace("+", "%2B");
+    }
+
+    @Override
+    public String combine(FileInfo file, String uploadId, Map<String, String> map) {
+        String fileUrl = super.combine(file, uploadId, map);
+        return fileUrl.replace("+", "%2B");
+    }
+
+    @Override
+    public String copyFile(FileInfo file, String oldFileKey, String oldBucketName) {
+        String fileUrl = super.copyFile(file, oldFileKey, oldBucketName);
+        return fileUrl.replace("+", "%2B");
+    }
+
 }
